55905a0f09681ae738f70f74a6cda57683739391
Nilanjan Jana <96487546+NilanjanJana16@users.noreply.github.com>
2023-07-25 17:39:09 +0530

    Added solution - LeetHub

---
+++ Anagram - GFG/anagram.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+from collections import Counter
+
+class Solution:
+    
+    #Function is to check whether two strings are anagram of each other or not.
+    def isAnagram(self,a,b):
+        #code here
+        
+        
+        if Counter(a) == Counter(b):
+            return True
+        return False
+
+        
+        #sort_a = sorted(a)
+        #sort_b = sorted(b)
+        
+        #comapre them
+        
+        #if sort_a == sort_b:
+            #return "YES"
+        #else:
+            #return "NO"
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        a,b=map(str,input().strip().split())
+        if(Solution().isAnagram(a,b)):
+            print("YES")
+        else:
+            print("NO") 
+# } Driver Code Ends
